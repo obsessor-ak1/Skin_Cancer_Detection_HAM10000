849f08c956f34ec845d25e4a29e7836e428bcef4
Aayush Kumar <aayushkumarfirst@gmail.com>
2025-09-05 18:44:21 +0530

    Workable state

--- exp_tools/trainer.py
+++ exp_tools/trainer.py
@@ -1,7 +1,127 @@
+import copy
+import math
+
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
 from exp_tools.basic_utils import predict, separate
+
+
+class History:
+    """Represent the history of the training process."""
+
+    def __init__(self, loggers=None):
+        self._history = {
+            "train": {"loss": []},
+            "val": {"loss": []}
+        }
+        self.loggers = loggers
+        self.__finished = False
+
+    def add_metric(self, name, metric, train=True):
+        """Adds a new training/validation metric for an epoch."""
+        if self.__finished:
+            raise AttributeError("The training is finished, cannot add more data.")
+        section = "train" if train else "val"
+        current_values = self._history[section].setdefault(name, [])
+        current_values.append(metric)
+        # TODO: Logger support
+
+    def get_metric(self, name, epoch=-1, train=True):
+        """Returns the value of a particular metric at a certain epoch."""
+        prefix = "train" if train else "val"
+        metric_value = self._history[prefix][name]
+        if isinstance(metric_value, list):
+            return metric_value[epoch]
+        else:
+            return {key: value[epoch] for key, value in metric_value.items()}
+
+    @staticmethod
+    def _vectorize_list_of_dict(list_of_dicts):
+        """Converts and returns a list of dicts to a dict of lists."""
+        keys = list_of_dicts[0].keys()
+        final_dict = {
+            key: [value_dict[key] for value_dict in list_of_dicts]
+            for key in keys
+        }
+        return final_dict
+
+    def _vectorize(self):
+        """Vectorizes all the metrics if they are list of dicts."""
+        for metric in self._history["train"]:
+            value = self._history["train"][metric]
+            if len(value) > 0 and isinstance(value[0], dict):
+                self._history["train"][metric] = History._vectorize_list_of_dict(value)
+                val_metrics = self._history["val"].get(metric, [])
+                if len(val_metrics) > 0:
+                    self._history["val"][metric] = History._vectorize_list_of_dict(val_metrics)
+
+    def finish(self):
+        """Finalizes the history and freezes the state."""
+        self._vectorize()
+        self.__finished = True
+
+    @property
+    def history(self):
+        """Returns the training history as a python dict."""
+        return copy.deepcopy(self._history)
+
+    @property
+    def epochs(self):
+        """Returns the number of epochs."""
+        return len(self._history["train"]["loss"])
+
+    def _generate_multiplot(self, axis, values, train=True, metric_title=None):
+        """Generates a single plot for the training history metric (2D)."""
+        x = range(self.epochs)
+        line_style = "--" if train else "-"
+        prefix = "train" if train else "val"
+        axis.set_xlabel("Epochs")
+        axis.set_ylabel(metric_title.capitalize())
+        cmap = plt.cm.get_cmap("tab10", len(values))
+        for i, (key, value) in enumerate(values.items()):
+            label = f"{prefix}_{metric_title}:{key}"
+            axis.plot(x, value, label=label, color=cmap(i), linestyle=line_style)
+        axis.legend(loc="best")
+        axis.set_title(f"{prefix.capitalize()} {metric_title.capitalize()}")
+
+    def _generate_single_plot(self, axis, metric_values, metric_title=None):
+        """Generates a single plot for the training history metric (1D)."""
+        x = range(self.epochs)
+        train_metric = metric_values["train"]
+        val_metric = metric_values.get("val", [])
+        axis.set_xlabel("Epochs")
+        axis.set_ylabel(metric_title.capitalize())
+        axis.plot(x, train_metric, label=f"train_{metric_title}", linestyle="-")
+        if len(val_metric) > 0:
+            axis.plot(x, val_metric, label=f"val_{metric_title}", linestyle="--")
+            axis.legend(loc="best")
+        axis.set_title(f"{metric_title.capitalize()}")
+
+    def plot_history(self):
+        """Plots the complete training history."""
+        num_plots = len(self._history["train"])
+        num_vectored_metrics = sum(isinstance(values, dict) for values in self._history["val"].values())
+        num_plots += num_vectored_metrics
+        num_rows = math.ceil(num_plots / 2)
+        fig, axes = plt.subplots(num_rows, 2, figsize=(12, num_rows * 5))
+        if not isinstance(axes, np.ndarray):
+            axes = np.array([axes])
+
+        axes = axes.flatten()
+        i = 0
+        for metric, train_metric in self._history["train"].items():
+            val_metric = self._history["val"].get(metric, dict())
+            if isinstance(train_metric, dict):
+                self._generate_multiplot(axes[i], train_metric, metric_title=metric)
+                if len(val_metric) > 0:
+                    i += 1
+                    self._generate_multiplot(axes[i], val_metric, False, metric_title=metric)
+            else:
+                metrics = {"train": train_metric, "val": val_metric}
+                self._generate_single_plot(axes[i], metrics, metric_title=metric)
+            i += 1
 
 
 class Trainer:
@@ -11,10 +131,7 @@ class Trainer:
         self._max_epochs = max_epochs
         self._device = torch.device(device)
         self.clip_grad = clip_grad
-        self.current_history = {
-            "train": dict(),
-            "val": dict()
-        }
+        self.current_history = History()
         self.metrics = metrics
         self.clip_val = clip_val
 
@@ -25,10 +142,11 @@ class Trainer:
         for epoch in range(self._max_epochs):
             print(f"Epoch {epoch + 1}/{self._max_epochs}")
             self._fit_epoch(model, loss_fn, optimizer, train_data)
-            print(f"\nTrain loss: {self.current_history['train']['loss'][-1]}")
+            print(f"\nTrain loss: {self.current_history.get_metric('loss')}")
             if val_data:
                 self._validate(model, loss_fn, val_data)
-                print(f"Val loss: {self.current_history['val']['loss'][-1]}")
+                print(f"Val loss: {self.current_history.get_metric('val', train=False)}")
+        self.current_history.finish()
 
     def _fit_epoch(self, model, loss_fn, optimizer, train_data):
         """Fits one single epoch."""
@@ -37,6 +155,7 @@ class Trainer:
         n_samples_processed = 0
         # Fitting the model for an epoch
         for batch, (X, y) in enumerate(train_data):
+            optimizer.zero_grad()
             X, y = X.to(device=self._device), y.to(self._device)
             y_hat = model(X)
             loss = loss_fn(y_hat, y)
@@ -50,8 +169,7 @@ class Trainer:
             Trainer.show_progress(batch + 1, num_batches)
 
         # Recording the loss
-        loss_hist = self.current_history["train"].setdefault("loss", [])
-        loss_hist.append(sum(loss_vals) / n_samples_processed)
+        self.current_history.add_metric("loss", sum(loss_vals) / n_samples_processed)
         # Recording the metrics
         self._record_metrics(model, train_data)
 
@@ -69,8 +187,7 @@ class Trainer:
                 sample_count += y.size(0)
                 batch_losses.append(loss)
         # Recording the loss
-        val_losses = self.current_history["val"].setdefault("loss", [])
-        val_losses.append(sum(batch_losses) / sample_count)
+        self.current_history.add_metric("loss", sum(batch_losses) / sample_count, False)
         # Recording the metrics
         self._record_metrics(model, val_data, False)
 
@@ -92,13 +209,11 @@ class Trainer:
         y_pred_total = np.concatenate(y_pred_total)
         # Recording the metrics
         for metric, fun in self.metrics.items():
-            metric_values = self.current_history[history_section].setdefault(metric, [])
-            metric_values.append(fun(y_true_total, y_pred_total))
+            self.current_history.add_metric(metric, fun(y_true_total, y_pred_total), train)
 
     def reset_history(self):
         """Resets the current history dictionary."""
-        self.current_history["train"].clear()
-        self.current_history["val"].clear()
+        self.current_history = History()
 
     @staticmethod
     def show_progress(current_batch, total_batches):
